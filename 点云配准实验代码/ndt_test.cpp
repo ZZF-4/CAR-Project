bc777326569ce252478e83ffadf8c032ecf163c6
sc <2213666375@qq.com>
2024-10-10 23:06:57 +0800

    改进ndt_test.cpp文件的内容，完成基于ICP的配准实验

--- 点云配准实验代码/ndt_test.cpp
+++ 点云配准实验代码/ndt_test.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-#include <thread>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/registration/ndt.h>
 #include <pcl/filters/approximate_voxel_grid.h>
 #include <pcl/visualization/pcl_visualizer.h>
-
-
-using namespace std::chrono_literals;
+#include <thread>
+#include <chrono>
 
 int main(int argc, char **argv)
 {
@@ -18,6 +16,7 @@ int main(int argc, char **argv)
         return (-1);
     }
     std::cout << "load " << target_cloud->size() << " data points from target_cloud" << endl;
+
     pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud(new pcl::PointCloud<pcl::PointXYZ>);
     if (pcl::io::loadPCDFile<pcl::PointXYZ>("../data/room_scan2.pcd", *source_cloud) == -1)
     {
@@ -25,28 +24,42 @@ int main(int argc, char **argv)
         return (-1);
     }
     std::cout << "load " << source_cloud->size() << " data points from source_cloud" << endl;
-    pcl::PointCloud<pcl::PointXYZ>::Ptr filter_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::ApproximateVoxelGrid<pcl::PointXYZ> approximate_voxel_filter;
 
+    pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::ApproximateVoxelGrid<pcl::PointXYZ> approximate_voxel_filter;
     approximate_voxel_filter.setLeafSize(0.3, 0.3, 0.3);
     approximate_voxel_filter.setInputCloud(source_cloud);
-    approximate_voxel_filter.filter(*filter_cloud);
-
-    std::cout << "Filter cloud contain " << filter_cloud->size() << "data points from source_cloud" << endl;
+    approximate_voxel_filter.filter(*filtered_cloud);
+    std::cout << "Filter cloud contain " << filtered_cloud->size() << "data points from source_cloud" << endl;
 
     Eigen::AngleAxisf init_rotation(0.69, Eigen::Vector3f::UnitZ());
-    Eigen::Translation3f init_translasition(1.0, 0, 0);
-    Eigen::Matrix4f init_guss = (init_translasition * init_rotation).matrix();
+    Eigen::Translation3f init_translation(1.0, 0, 0);
+    Eigen::Matrix4f init_guess = (init_translation * init_rotation).matrix();
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr output_cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
-    // =======================   ndt   =======================
-    // 补全相关代码
+    // NDT 配准
+    pcl::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ> ndt;
+    ndt.setTransformationEpsilon(0.001);  // 更严格的变换epsilon
+    ndt.setStepSize(0.001);                 // 更小的步长
+    ndt.setResolution(0.05);                // 较高的分辨率
+    ndt.setMaximumIterations(100);         // 更多的迭代次数
+    ndt.setInputSource(filtered_cloud);
+    ndt.setInputTarget(target_cloud);
 
-    
-    // =======================   ndt   =======================
-    
+    ndt.align(*output_cloud, init_guess);
 
+    if (ndt.hasConverged())
+    {
+        std::cout << "NDT has converged, score is " << ndt.getFitnessScore() << std::endl;
+        std::cout << "Transformation is:\n" << ndt.getFinalTransformation() << std::endl;
+    }
+    else
+    {
+        std::cout << "NDT did not converge." << std::endl;
+    }
+
+    // 可视化
     pcl::visualization::PCLVisualizer::Ptr viewer_final(new pcl::visualization::PCLVisualizer("3D viewer"));
     viewer_final->setBackgroundColor(0, 0, 0);
     pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> target_color(target_cloud, 255, 0, 0);
@@ -62,7 +75,9 @@ int main(int argc, char **argv)
     while (!viewer_final->wasStopped())
     {
         viewer_final->spinOnce(100);
-        std::this_thread::sleep_for(100ms);
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+
     return 0;
 }
+
